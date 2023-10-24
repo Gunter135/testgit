9a4637573fbefd2593bcdcbf7c77323eedc87981
unknown <evdokimov0711@mail.ru>
2023-10-24 14:02:35 +0300

    dwlidadjiwa
    sdujksnfse

--- j.java
+++ j.java
@@ -1,5 +1,13 @@
 public static Cls(){
 	public static void main(args[] String){
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+
 	}
 }
